0f467a68c9a0fca27c4238cf8d9826eb0b5edf9c
Ronak Babariya <ronakbabariya555@gmail.com>
2024-05-08 18:16:49 +0530

    jwt implementation

--- CRUD/CRUD/urls.py
+++ CRUD/CRUD/urls.py
@@ -3,13 +3,17 @@ from django.contrib import admin
 from django.urls import path,include
 from api import views
 from rest_framework.routers import DefaultRouter 
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView,TokenVerifyView
 
 router = DefaultRouter()
 
-router.register('crud_api',views.Crudapi,basename='crud')
-router.register('readonly_api',views.Read_only_api,basename='read_only')
+router.register('crud_api', views.Crudapi,basename='crud')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include(router.urls))
+    path('',include(router.urls)),
+
+    path('gettoken/',TokenObtainPairView.as_view(),name='token_obtain_pair'),
+    path('refreshtoken/', TokenRefreshView.as_view(),name='refresh_token'),
+    path('verifytoken/',TokenVerifyView.as_view(),name='Verify_token'),
 ]
--- CRUD/api/views.py
+++ CRUD/api/views.py
@@ -1,18 +1,16 @@
 from .serializers import Student_serializer
 from .models import Student
 from rest_framework import viewsets
-
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 
 class Crudapi(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = Student_serializer
 
-    
-class Read_only_api(viewsets.ReadOnlyModelViewSet):
-    queryset = Student.objects.all()
-    serializer_class = Student_serializer
-
-
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
 
     
+

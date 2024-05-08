78bcab936b0101702ab72f0fa280bb95cfcf51a0
Ronak Babariya <ronakbabariya555@gmail.com>
2024-05-08 10:50:16 +0530

    concrere view class

--- CRUD/api/views.py
+++ CRUD/api/views.py
@@ -1,37 +1,18 @@
-
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
 from .serializers import Student_serializer
 from .models import Student
-from rest_framework.generics import GenericAPIView
-from rest_framework.mixins import ListModelMixin,CreateModelMixin,UpdateModelMixin,RetrieveModelMixin,DestroyModelMixin
+from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
 
 # Create your views here.
 
-class List_Create_api(GenericAPIView,ListModelMixin,CreateModelMixin):
+class List_Create_api(ListCreateAPIView):
     queryset = Student.objects.all()
     serializer_class = Student_serializer
 
-    def get(self,request,*args,**kwargs):
-        return self.list(request,*args,**kwargs) 
     
-    def post(self,request,*args,**kwargs):
-        return self.create(request,*args,**kwargs) 
-    
-class Retrieve_Update_Delete_api(GenericAPIView,RetrieveModelMixin,UpdateModelMixin,DestroyModelMixin):
+class Retrieve_Update_Delete_api(RetrieveUpdateDestroyAPIView):
     queryset = Student.objects.all()
     serializer_class = Student_serializer
 
-    def get(self,request,*args,**kwargs):
-        return self.retrieve(request,*args,**kwargs) 
-
-    def put(self,request,*args,**kwargs):
-        return self.update(request,*args,**kwargs) 
-
-    def delete(self,request,*args,**kwargs):
-        return self.destroy(request,*args,**kwargs) 
-    
 
 
     

7933f8093b9f6925b997302acd8c8dee77efdcf6
Ronak Babariya <ronakbabariya555@gmail.com>
2024-05-09 14:01:11 +0530

    throttling

--- CRUD/api/views.py
+++ CRUD/api/views.py
@@ -2,7 +2,8 @@ from .serializers import Student_serializer
 from .models import Student
 from rest_framework import viewsets
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.throttling import AnonRateThrottle, UserRateThrottle
 # Create your views here.
 
 class Crudapi(viewsets.ModelViewSet):
@@ -10,7 +11,7 @@ class Crudapi(viewsets.ModelViewSet):
     serializer_class = Student_serializer
 
     authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticated]
-
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    throttle_classes = [AnonRateThrottle,UserRateThrottle]
     
 
